e87d4a88fcc23303de141907a94c78c99502556c
Vitali Orlioglo <vitalik.orlioglo@gmail.com>
2019-11-21 16:17:11 +0100

    deleteUserById

--- src/dao/UserDAO.java
+++ src/dao/UserDAO.java
@@ -16,7 +16,7 @@ public class UserDAO {
 	public void saveUser(User user) {
 		Session session = sf.getCurrentSession();
 		session.beginTransaction();
-		session.save(user);
+		session.save(user); // saveOrUpdate
 		session.getTransaction().commit(); // jetzt save
 		
 		boolean ok = session.getTransaction().getStatus() == TransactionStatus.COMMITTED;
@@ -50,6 +50,40 @@ public class UserDAO {
 		session.close();
 		
 		return user;
+	}
+	
+	public boolean deleteUserById(int id) {
+		Session session = sf.getCurrentSession();
+		session.beginTransaction();
+		User user = session.load(User.class, id);// load erzeugt Exception, wenn nicht gefunden
+		if (user != null) {
+			session.delete(user);
+			session.getTransaction().commit();
+		}
+		
+		boolean ok = session.getTransaction().getStatus() == TransactionStatus.COMMITTED;
+		System.out.println("deleted " + ok);
+		
+		session.close();
+		return ok;
+	}
+	
+	public boolean updatePassword(int id, String newPassword) {
+		Session session = sf.getCurrentSession();
+		session.beginTransaction();
+		User user = session.get(User.class, id);// get ist wie load(aber mit null)
+		if (user != null) {
+			user.setPasswort(newPassword);
+			session.update(user);
+			session.getTransaction().commit();
+		}
+		
+		boolean ok = session.getTransaction().getStatus() == TransactionStatus.COMMITTED;
+		System.out.println("updated " + ok);
+		
+		session.close();
+		
+		return ok;
 	}
 	
 	public static void main(String[] args) {
